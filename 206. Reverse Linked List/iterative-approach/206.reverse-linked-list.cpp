3c4bf20a771cb98f5e995e92d4a4483cb61e8d13
Mourad NOUAILI <mouradnouaili1@gmail.com>
2021-09-29 20:27:26 +0100

    Add files via upload

---
+++ 206. Reverse Linked List/iterative-approach/206.reverse-linked-list.cpp
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head != nullptr){
+        
+            if(head->next == nullptr)
+                return head;
+
+            ListNode* t1 = head->next;
+            ListNode* t2 = nullptr;
+            
+            if (t1->next != nullptr)
+                t2 = t1->next;
+
+            head->next = nullptr;
+
+            while (head != t1) {
+                t1->next = head;
+                head = t1;
+                if (t2 != nullptr) {
+                    t1 = t2;
+                    if (t2->next != nullptr)
+                        t2 = t2->next;
+                }
+            }
+            
+            t1 = t2 = nullptr;
+            delete t1;
+            delete t2;
+        }
+
+        return head;
+        
+    }
+};
+// @lc code=end
+
